58d47ba321600d3474d140faf39254198252fa1a
Antonio <antoniotornero@Antonios-MacBook-Pro.local>
2021-11-10 12:12:05 +0000

    first commit

---
+++ src/formativetask1/ApplicationRunner.java
@@ -0,0 +1,13 @@
+package formativetask1;
+
+import java.io.File;
+
+public class ApplicationRunner {
+
+    public static void main(String[] args) {
+        
+        String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
+
+    }
+
+}
