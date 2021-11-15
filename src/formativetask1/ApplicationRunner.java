a4e35e13d9ee6d6d52e28a3e84930b47861fb953
Antonio <antoniotornero@Antonios-MacBook-Pro.local>
2021-11-15 17:09:05 +0000

    adding work 1

--- src/formativetask1/ApplicationRunner.java
+++ src/formativetask1/ApplicationRunner.java
@@ -1,12 +1,63 @@
 package formativetask1;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+
+import java.util.Scanner;
 
 public class ApplicationRunner {
 
     public static void main(String[] args) {
-        
+
         String dataFile = System.getProperty("user.dir") + File.separator + "datafile.txt";
+
+        String opFile = System.getProperty("user.dir") + File.separator + "result.txt";
+
+        File fileObj = new File(dataFile);
+
+        Scanner input = null;
+
+        PrintWriter pw = null;
+
+        try {
+
+            input = new Scanner(fileObj);
+
+            pw = new PrintWriter(new File(opFile));
+
+            while (input.hasNext()) {
+
+                String line = input.nextLine().trim();
+
+                //System.out.println(line);
+                
+                String[] items = line.split(" ");
+                
+                for(String s: items){
+                
+                    System.out.println(s);
+                
+                }
+                
+                //pw.println(line);
+
+            }
+
+        } catch (FileNotFoundException fne) {
+
+            System.out.println("File not founded....");
+            System.exit(0);
+
+        } finally {
+
+            System.out.println("Fin of the Block");
+
+            input.close();
+
+            pw.close();
+
+        }
 
     }
 
